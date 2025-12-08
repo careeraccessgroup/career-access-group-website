3e24bd558cc1743b3f15d6bf69ed275ed16d5526
uwase sophie <uwasesophie91@gmail.com>
2025-12-08 18:06:12 +0200

    project structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ function App() {
 
   return (
     <>
-      
+      <div className="bg-primary-dark text-textcolor-300">
+  Header
+</div>
+
+<button className="bg-secondary-orange hover:bg-secondary-hover-100 text-white px-4 py-2 rounded">
+  Join Now
+</button>
+
+<section className="bg-bgcolor-200 text-textcolor-100">
+  Features section
+</section>
+
     </>
   )
 }
