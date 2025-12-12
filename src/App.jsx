bfd6ee8da5d513959593d7df5d75b7b570933b16
uwase sophie <uwasesophie91@gmail.com>
2025-12-12 09:04:24 +0200

    project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
+import NotFound from './pages/NotFount.jsx'
+
 function App() {
-
-  return (
-    <>
-      <div className="bg-primary-dark text-textcolor-300">
-  Header
-</div>
-
-<button className="bg-secondary-orange hover:bg-secondary-hover-100 text-white px-4 py-2 rounded">
-  Join Now
-</button>
-
-<section className="bg-bgcolor-200 text-textcolor-100">
-  Features section
-</section>
-
-    </>
-  )
+  // During development show a single placeholder page
+  return <NotFound />
 }
 
 export default App
